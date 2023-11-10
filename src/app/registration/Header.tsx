ebfd132f004c345f723c515e9830bbbc6aa989b1
Yuhe <gavinbianyuhe@hotmail.com>
2023-11-10 04:18:22 -0500

    Create Header.tsx

---
+++ src/app/registration/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <img src="/path-to-shine-ai.png" alt="shine-ai" />
+    </div>
+  );
+}
+
+export default Header;
