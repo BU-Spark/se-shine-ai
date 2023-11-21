c2f09c628bc2f38a023db34684d0046454032906
yuhe bian <yuheb@bu.edu>
2023-11-21 18:19:42 -0500

    change file name index to page

---
+++ src/app/registration/page.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import React, { useState } from 'react';
+import Header from './Header';
+import Question from './Question';
+import Options from './Options';
+import ProgressBar from './ProgressBar';
+import SubmitButton from './SubmitButton';
+import styles from './page.module.css';
+
+const Home: React.FC = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [selectedOptions, setSelectedOptions] = useState<Set<string>>(new Set());
+
+  const handleBackClick = () => {
+    setCurrentStep(prev => prev > 1 ? prev - 1 : 1);
+  };
+
+  const handleSubmitClick = () => {
+    setCurrentStep(prev => prev < 5 ? prev + 1 : 5); // Increment the current step to a max of 5
+  };
+  const handleSkipClick = () => {
+  setCurrentStep(prev => (prev < 5 ? prev + 1 : prev));
+  };
+  const toggleOption = (option: string) => {
+    setSelectedOptions(prevSelectedOptions => {
+      const newSelectedOptions = new Set(prevSelectedOptions);
+      if (newSelectedOptions.has(option)) {
+        newSelectedOptions.delete(option);
+      } else {
+        newSelectedOptions.add(option);
+      }
+      return newSelectedOptions;
+    });
+  };
+  const subtitles = [
+    "It’s been around...", 
+    "I want to...", 
+    "I am interested in...", 
+    "I would say...",
+    "I want to learn" 
+  ];
+
+
+  return (
+    <div className={styles.container}>
+      <Header step={currentStep} onBackClick={handleBackClick}/>
+      <div className={styles.backAndProgressBar}>
+        <div className={styles.progressBarContainer}>
+          <div
+            className={styles.progressBarFiller}
+          />
+        </div>
+      </div>
+      <Question step={currentStep} subtitle={subtitles[currentStep - 1]} />
+      <Options 
+        step={currentStep} 
+        selectedOptions={selectedOptions} 
+        toggleOption={toggleOption} 
+      />
+      <SubmitButton onClick={handleSubmitClick} />
+      {currentStep === 4 && (
+        <button className={styles.skipButton} onClick={handleSkipClick}>
+          or skip this question...
+        </button>
+        )}
+    </div>
+  );
+}
+
+export default Home;
