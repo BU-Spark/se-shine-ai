cbd53b6014e43aadea8b3206bf5d8f0308b754b3
Jason Oh <jasonoh1998@gmail.com>
2023-12-08 13:39:00 -0500

    i/o bug fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import TopNavbar from '@/components/dashboard/TopNavbar';
-import BottomNavbar from '@/components/dashboard/BottomNavBar';
+import BottomNavbar from '@/components/dashboard/BottomNavbar';
 
 export default function DashboardLayout(
     {children,} 
